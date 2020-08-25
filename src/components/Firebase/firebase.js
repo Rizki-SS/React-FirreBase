b91315391dbda10c171352c119184816de06f961
isak <muhammadisak.rizki@gmail.com>
2020-08-25 16:54:14 +0700

    singUp

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,24 +1,25 @@
 import app from "firebase/app";
 import 'firebase/auth';
 
-const prodConfig = {
-    apiKey: process.env.REACT_APP_PROD_API_KEY,
-    authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROD_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+// const prodConfig = {
+//     apiKey: process.env.REACT_APP_PROD_API_KEY,
+//     authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+//     projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+// };
+
+const config = {
+    apiKey: "AIzaSyDfx2XNLXE8YKQDSfPE7zP3_7Vus6Xa0SA",
+    authDomain: "wise-scene-252504.firebaseapp.com",
+    databaseURL: "https://wise-scene-252504.firebaseio.com",
+    projectId: "wise-scene-252504",
+    storageBucket: "wise-scene-252504.appspot.com",
+    messagingSenderId: "958656594783",
+    appId: "1:958656594783:web:437dc51f7fce1c36a4ecea",
+    measurementId: "G-K4GVJE3QEL"
 };
-const devConfig = {
-    apiKey: process.env.REACT_APP_DEV_API_KEY,
-    authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
-    projectId: process.env.REACT_APP_DEV_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
-};
-const config =
-    process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 class Firebase {
     constructor() {
