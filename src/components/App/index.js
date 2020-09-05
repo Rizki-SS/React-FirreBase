d5b7bc63716062c5e0e6fa18e5aa7bf378d19b0f
isak <muhammadisak.rizki@gmail.com>
2020-09-05 11:05:06 +0700

    password manajemnet

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,9 +7,9 @@ import Navigation from "../Navigation";
 // import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
-// import PasswordForgetPage from '../PasswordForget';
+import PasswordForgetPage from '../PasswordForget';
 // import HomePage from '../Home';
-// import AccountPage from '../Account';
+import AccountPage from '../Account';
 // import AdminPage from '../Admin'
 
 import * as ROUTES from '../../constants/routes'
@@ -22,9 +22,9 @@ const App = () => (
             {/* <Route exact path={ROUTES.LANDING} component={LandingPage} /> */}
             <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
             <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-            {/* <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} /> */}
+            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
             {/* <Route path={ROUTES.HOME} component={HomePage} /> */}
-            {/* <Route path={ROUTES.ACCOUNT} component={AccountPage} /> */}
+            <Route path={ROUTES.ACCOUNT} component={AccountPage} />
             {/* <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
         </div>
     </Router>
